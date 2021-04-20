180a0450c41e8dbff913139e014fe42110bba9b5
Steyler <steylerap@gmail.com>
2021-04-20 01:12:25 -0600

    -A

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :bookmarks
   resources :kinds
   resources :categories
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'bookmarks#index'
+  get 'categories/:id/api_config', to: 'categories#api', as: 'api'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -30,26 +30,26 @@ Category.create(
 )
 Bookmark.create(
     [
-        { title: "Creacion un test ", url:"https://www3.animeflv.net/anime/one-piece-tv", kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Tecnología").id },
-        { title: "Boku no Hero Academia", url: "https://www3.animeflv.net/anime/boku-no-hero-academia-2016", kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Innovaciones").id },
-        { title: "One piece", url:"https://www3.animeflv.net/anime/one-piece-tv", kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Tecnología").id },
-        { title: "Konosuba", url: "https://www3.animeflv.net/anime/kono-subarashii-sekai-ni-shukufuku-wo",kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Deportes").id },
-        { title: "Solo leveling", url: "https://lectortmo.com/library/manhwa/41512/solo-leveling",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Tecnología").id },
-        { title: "Spy x Family", url: "https://lectortmo.com/library/Video/43882/spy-x-family",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Innovaciones").id },
-        { title: "Owari no Seraph", url: "https://lectortmo.com/library/Video/8190/Owari-no-Seraph",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Tecnología").id },
-        { title: "Jujutsu Kaisen", url: "https://www3.animeflv.net/anime/jujutsu-kaisen-tv",kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Tecnología").id },
-        { title: "Jujutsu Kaisen", url: "https://lectortmo.com/library/Video/36989/jujutsu-kaisen",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Tecnología").id },
-        { title: "Junk the Black Shadow", url: "https://lectortmo.com/library/Video/37907/Junk-the-Black-Shadow",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Nacionales").id },
-        { title: "Log Horizon", url: "https://www3.animeflv.net/anime/log-horizon",kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Deportes").id },
-        { title: "Vinland Saga", url: "https://www3.animeflv.net/anime/vinland-saga",kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Tecnología").id },
-        { title: "Fate Stay Night", url: "https://www3.animeflv.net/anime/fate-stay-night",kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Nacionales").id },
-        { title: "The Irregular at Magic High School", url: "https://www3.animeflv.net/anime/mahouka-koukou-no-rettousei",kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Innovaciones").id },
-        { title: "Hunter X Hunter", url: "https://lectortmo.com/library/Video/212/hunter-x-hunter",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Tecnología").id },
-        { title: "Gantz", url: "https://lectortmo.com/library/Video/8804/gantz",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Tecnología").id },
-        { title: "I am a Hero", url: "https://lectortmo.com/library/Video/6102/i-am-a-hero",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Deportes").id },
-        { title: "Neo Genesis Evangelion", url: "https://www3.animeflv.net/anime/neon-genesis-evangelion",kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Tecnología").id },
-        { title: "Slam Dunk", url: "https://lectortmo.com/library/Video/11580/slam-dunk",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Innovaciones").id },
-        { title: "Higurashi no Naku Koro Ni", url: "https://www3.animeflv.net/anime/higurashi-no-naku-koro-ni",kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Innovaciones").id }
+        { title: "Asistentes digitales con voz, la nueva compañía ", url:"https://picsum.photos/id/237/200/300", kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Tecnología").id },
+        { title: "Inteligencia Artificial (IA), el año de su madurez en los negocios", url: "https://picsum.photos/seed/picsum/200/300", kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Innovaciones").id },
+        { title: "Internet de las cosas (IoT), tu casa conectada", url:"https://picsum.photos/200/300?grayscale", kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Tecnología").id },
+        { title: "Automatización de procesos Robóticos (RPA), como una realidad", url: "https://www.flightradar24.com/51.53,0/8",kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Deportes").id },
+        { title: "Seguridad de datos o ciberseguridad", url: "https://www.music-map.com/g",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Tecnología").id },
+        { title: "La migración a la multi-nube", url: "https://traintimes.org.uk/map/tube/schematic/",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Innovaciones").id },
+        { title: "Analítica y minería de datos como el foco en muchas profesiones", url: "https://www.spacejam.com/jam.htm",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Tecnología").id },
+        { title: "Realidad Virtual y Realidad Aumentada (RA", url: "kaisen-tv",kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Tecnología").id },
+        { title: "Comercio en línea dentro de las preferencias más altas", url: "https://picsum.photos/id/0/info",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Tecnología").id },
+        { title: "BlockChain, más que bloques", url: "https://lectortmo.com/library/Video/37907/Junk-the-Black-Shadow",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Nacionales").id },
+        { title: "Log Horizon", url: "https://www.zoomquilt.org/",kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Deportes").id },
+        { title: "Llegada de la red 5G, red a toda velocidad", url: "https://www.music-map.com/",kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Tecnología").id },
+        { title: "Chatbot y respuestas rápidas", url: "http://hyperphysics.phy-astr.gsu.edu/hbase/index.html",kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Nacionales").id },
+        { title: "Creacion de atomo", url: "https://www.google.com/maps/d/u/0/viewer?mid=1Z1dI8hoBZSJNWFx2xr_MMxSxSxY&hl=en_US&ll=-3.539794309791734%2C48.86813719999998&z=1",kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Innovaciones").id },
+        { title: "Microsoft desea invertir en Costa Rica", url: "https://www.mapcrunch.com/",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Tecnología").id },
+        { title: "IBM invierte en chatbots e IA", url: "https://picsum.photos/200/300.jpg",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Tecnología").id },
+        { title: "I am a Hero", url: "https://picsum.photos/200/300.webp",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Deportes").id },
+        { title: "Splunk for analyze", url: "https://picsum.photos/id/870/200/300?grayscale&blur=2",kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Tecnología").id },
+        { title: "AT&T building", url: "https://picsum.photos/200/300/?blur=2",kind_id: Kind.find_by(name: "Video").id, category_id: Category.find_by(name: "Innovaciones").id },
+        { title: "Carlillos prueba", url: "https://picsum.photos/200/300/?blur",kind_id: Kind.find_by(name: "Artículo").id, category_id: Category.find_by(name: "Innovaciones").id }
 
     ]
 )
